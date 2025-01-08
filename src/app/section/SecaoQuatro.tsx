9109ea1230bde31fbd5ebcb9e8a1c74b70ace90f
diegomcs <diegocs.art@gmail.com>
2025-01-08 19:55:49 -0300

    Modificações em SecaoUm.tsx

--- src/app/section/SecaoQuatro.tsx
+++ src/app/section/SecaoQuatro.tsx
@@ -5,7 +5,7 @@ import Formulario from "./form"; // Importe o componente Formulario
 export default function SecaoQuatro() {
   return (
     <div>
-      <div className="w-full relative flex flex-col sm:flex-row items-center justify-between p-16 text-center md:text-center text-2xl text-verde font-work-sans gap-8">
+      <div className="w-full relative flex flex-col sm:flex-row items-center justify-between p-16 text-center md:text-left text-2xl text-verde font-work-sans gap-8">
         {/* Seção Texto */}
         <div className="texto w-full sm:w-1/2 mb-8 sm:mb-0">
           <b className="uppercase flex items-center w-full">
@@ -14,7 +14,7 @@ export default function SecaoQuatro() {
         </div>
 
         {/* Seção do Formulário */}
-        <div className="formulario flex w-full items-center justify-center sm:w-1/2">
+        <div className="formulario flex w-full items-center justify-center p-30 sm:w-1/2">
           <Formulario /> 
         </div>
       </div>
