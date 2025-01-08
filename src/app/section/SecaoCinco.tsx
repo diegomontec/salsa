79770dd3e221e132075c38e2eaf35c573f04f5b8
diegomcs <diegocs.art@gmail.com>
2025-01-08 19:39:41 -0300

    Modificações em SecaoUm.tsx

--- src/app/section/SecaoCinco.tsx
+++ src/app/section/SecaoCinco.tsx
@@ -9,14 +9,11 @@ export default function SecaoCinco() {
   ];
 
   return (
-    <div className="w-full bg-verde flex flex-col items-center justify-center p-8 gap-8 md:gap-2 sm:gap-2 text-center">
-      {/* Título Responsivo */}
+    <div className="w-full bg-verde flex flex-col items-center justify-center p-8 md:p-16 gap-8 md:gap-16 text-center">
       <b className="relative uppercase text-lg md:text-2xl text-white font-work-sans">
         Sua rotina ganha novos motivos para ser ainda mais incrível
       </b>
-
-      {/* Carrossel */}
-      <div className="w-full h-full overflow-hidden">
+      <div className="w-full max-w-4xl overflow-hidden">
         <Carousel images={images} />
       </div>
     </div>
