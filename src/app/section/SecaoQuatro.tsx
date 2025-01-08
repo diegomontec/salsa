b74e472df741529191018fcc7925f756cbd86a2b
diegomontec <diegocs.art@gmail.com>
2025-01-08 14:39:30 -0300

    review

--- src/app/section/SecaoQuatro.tsx
+++ src/app/section/SecaoQuatro.tsx
@@ -1,15 +1,31 @@
+"use client"
+
+import { useEffect } from "react";
+
 export default function SecaoQuatro() {
+  useEffect(() => {
+    const script = document.createElement("script");
+    script.src = "https://cdn.bitrix24.com.br/b32099025/crm/form/loader_1.js";
+    script.async = true;
+    script.setAttribute("data-b24-form", "inline/1/cliewn");
+    script.setAttribute("data-skip-moving", "true");
+
+    document.body.appendChild(script);
+
+    return () => {
+      document.body.removeChild(script);
+    };
+  }, []);
+
   return (
     <div>
-      <div className="w-full bg-orange-400 relative flex flex-col sm:flex-row items-start justify-start p-16 text-left text-[1.75rem] text-verde font-work-sans">
+      <div className="w-full relative flex flex-col sm:flex-row items-start justify-start p-16 text-left text-[1.75rem] text-verde font-work-sans">
         <div className="texto">
           <b className=" uppercase flex items-center w-full">
             Cadastre-se e receba a apresentação completa do projeto
           </b>
         </div>
-        <div className="formulario flex-1">
-          form
-        </div>
+        <div className="formulario flex-1"></div>
       </div>
     </div>
   );
