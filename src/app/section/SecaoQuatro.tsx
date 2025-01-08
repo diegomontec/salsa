2a605a73cae01b8899ab35d4a3b4b8895caa9827
diegomontec <diegocs.art@gmail.com>
2025-01-08 11:09:20 -0300

    ResponsividadeSecaoCincoSeis

--- src/app/section/SecaoQuatro.tsx
+++ src/app/section/SecaoQuatro.tsx
@@ -1,11 +1,13 @@
 export default function SecaoQuatro() {
   return (
     <div>
-      <div className="w-full relative flex flex-row md:flex-col items-start justify-start py-[8rem] px-[4.375rem] box-border text-left text-[1.75rem] text-verde font-work-sans">
-        <b className="leading-[1.75rem] flex items-center w-[49.375rem]">
-          Cadastre-se e receba a apresentação completa!
-        </b>
-        <div className="formulario">
+      <div className="w-full relative flex flex-col sm:flex-row items-start justify-start py-[8rem] px-[4.375rem] box-border text-left text-[1.75rem] text-verde font-work-sans">
+        <div className="texto mb-4 sm:mb-0">
+          <b className="leading-[1.75rem] flex items-center w-full">
+            Cadastre-se e receba a apresentação completa!
+          </b>
+        </div>
+        <div className="formulario flex-1">
           form
         </div>
       </div>
--- src/app/section/SecaoTres.tsx
+++ src/app/section/SecaoTres.tsx
@@ -4,16 +4,18 @@ export default function SecaoTres() {
   return (
     <div>
       <div className="w-full relative flex flex-col items-start justify-start py-[8rem] px-[4.375rem] box-border gap-[4rem] text-left text-[1.75rem] text-verde font-work-sans">
-        <b className="w-full relative uppercase inline-block">
+        <b className=" uppercase">
           UM MERGULHO À BEIRA-MAR DE GUAXUMA
         </b>
-        <Image
-          className="w-full relative rounded-lg h-[42.938rem] object-cover"
-          width={1301}
-          height={687}
-          alt=""
-          src="/Piscina.png"
-        />
+        <div className="w-full">
+          <Image
+            className="rounded-lg object-cover w-full h-auto"
+            width={1301}
+            height={687}
+            alt="Piscina"
+            src="/Piscina.png"
+          />
+        </div>
       </div>
     </div>
   );
